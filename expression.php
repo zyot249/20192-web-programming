52b080a78ec5657dfbec6fad1271692c1c5ecbc9
Zyot249 <zyot249@gmail.com>
2020-03-10 09:42:56 +0700

    example of an expression

---
+++ expression.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Expression</title>
+    </head>
+    <body>
+        <?php
+            $gr1 = 50;
+            $gr2 = 100;
+            $gr3 = 75;
+            $aver = ($gr1 + $gr2 + $gr3)/3;
+            print "The average is $aver";
+        ?>
+    </body>
+</html>
